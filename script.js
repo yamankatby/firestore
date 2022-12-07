fd1a05248e95a87cfbc0da00dc84cda2490fff20
Yaman Katby <m.yaman.katby@gmail.com>
2022-12-07 12:55:48 +0300

    add firebase

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hi, from web app!");
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Initialize Firebase
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeA_4EtvPNBMh0jbA-zWNoeEsX6ZhZSzs",
+  authDomain: "fruits-d0078.firebaseapp.com",
+  projectId: "fruits-d0078",
+  storageBucket: "fruits-d0078.appspot.com",
+  messagingSenderId: "69757055872",
+  appId: "1:69757055872:web:b9ef7274fb60e37f894765",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
